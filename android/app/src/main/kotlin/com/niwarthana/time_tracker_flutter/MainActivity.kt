7e2b29d137739aa77c9eda1708ef9b3d6330fd7e
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-06-08 14:49:23 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/niwarthana/time_tracker_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.niwarthana.time_tracker_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/app/sign_in/sign_in_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
