53ec845f41dc8c3e59eed75041ebcf738a85dfd9
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-06-10 00:12:36 +0530

    add column

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget{
+class SignInPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
-        elevation: 10.0,
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Container _buildContent() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      color: Colors.amberAccent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          )
+        ],
       ),
     );
   }
